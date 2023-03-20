b3703da19eacd6769b76c4e62aa4a0a6fb35a39e
TatianaSteshakova <65027776+TatianaSteshakova@users.noreply.github.com>
2023-03-20 23:30:16 +0300

    Add files via upload

---
+++ Homework9.2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+// Сравниваем строки по ASCII-кодам букв
+bool compare(string s1, string s2) {
+    for (int i = 0; i < min(s1.length(), s2.length()); i++) {
+        if (s1[i] < s2[i]) {
+            return true;
+        }
+        else if (s1[i] > s2[i]) {
+            return false;
+        }
+    }
+    return s1.length() < s2.length();
+}
+
+int main()
+{   
+    system("chcp 1251"); // Без этого нормально не выводятся строки, setlocale не помогает
+    int n;
+
+    cout << "Введите количество элементов в массиве: " << endl;
+    cin >> n;
+
+    string* mas = new string[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Введите фамилию в ячейку mas[" << i + 1 << "]: " << endl;
+        cin >> mas[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (!compare(mas[i], mas[j]))
+            {
+                swap(mas[i], mas[j]); // Меняем местами
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << mas[i] << endl;
+    }
+
+}
